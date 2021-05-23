57abc769490e92fea9cf992c295d5112c2120f96
MahmudulAlam <mdmahmudulalam007@gmail.com>
2021-05-22 23:15:24 -0400

    added new figures
    changed eps files

--- plot/prediction plot default.py
+++ plot/prediction plot default.py
@@ -8,18 +8,18 @@ sns.set_style('darkgrid')
 labels = ['Not Taken', 'Taken', 'Bimod', '2lev', 'Combined Bimod & 2lev']
 
 # Branch Address
-# bar1 = [41.02, 55.78, 53.13, 55.78, 56.85, 58.11]
-# bar2 = [75.37, 66.14, 68.91, 66.14, 66.49, 65.08]
-# bar3 = [71.48, 88.38, 91.47, 88.38, 89.78, 94.15]
-# bar4 = [65.74, 85.69, 89.71, 85.69, 88.73, 93.03]
-# bar5 = [73.24, 89.79, 92.45, 89.79, 92.60, 95.72]
-
-# Branch Direction
 bar1 = [41.02, 55.78, 53.13, 55.78, 56.85, 58.11]
 bar2 = [75.37, 66.14, 68.91, 66.14, 66.49, 65.08]
-bar3 = [83.98, 91.16, 94.12, 91.16, 91.04, 94.72]
-bar4 = [77.96, 88.69, 92.42, 88.69, 90.16, 93.61]
-bar5 = [83.43, 92.15, 94.57, 92.15, 93.71, 96.27]
+bar3 = [71.48, 88.38, 91.47, 88.38, 89.78, 94.15]
+bar4 = [65.74, 85.69, 89.71, 85.69, 88.73, 93.03]
+bar5 = [73.24, 89.79, 92.45, 89.79, 92.60, 95.72]
+
+# Branch Direction
+# bar1 = [41.02, 55.78, 53.13, 55.78, 56.85, 58.11]
+# bar2 = [75.37, 66.14, 68.91, 66.14, 66.49, 65.08]
+# bar3 = [83.98, 91.16, 94.12, 91.16, 91.04, 94.72]
+# bar4 = [77.96, 88.69, 92.42, 88.69, 90.16, 93.61]
+# bar5 = [83.43, 92.15, 94.57, 92.15, 93.71, 96.27]
 
 for n, bar in enumerate(zip(bar1, bar2, bar3, bar4, bar5)):
     fig, ax = plt.subplots(figsize=[15, 15])
@@ -42,5 +42,5 @@ for n, bar in enumerate(zip(bar1, bar2, bar3, bar4, bar5)):
     plt.legend(fontsize=22, loc='upper right')
     plt.grid(True)
 
-    plt.savefig('../figure1/branch direction prediction/' + str(n + 1) + '.png', bbox_inches="tight", pad_inches=0)
-    plt.savefig('../figure1/branch direction prediction/' + str(n + 1) + '.eps', bbox_inches="tight", pad_inches=0)
+    plt.savefig('../figure1/branch address prediction/' + str(n + 1) + '.png', bbox_inches="tight", pad_inches=0)
+    plt.savefig('../figure1/branch address prediction/' + str(n + 1) + '.eps', bbox_inches="tight", pad_inches=0)
